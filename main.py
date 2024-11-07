c539cb4b03d161d36d99c2d8a6c07b1d3524aea8
muhammad subhan <muhammedsubhan692@gmail.com>
2024-11-08 02:51:16 +0500

    changes and bugs fixed

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-print("*" * 15 ,"To-Do List", "*" * 15)
+import os
+
+print("*" * 15, "To-Do List", "*" * 15)
 print("\n")
 
 todo_list = []
 
-title=""
-desc = ""
-
-
-
-
 def create_todo_list():
-    
     while True:
+        title = input("To-do List title (type 'exit' to stop adding): ")
+        if title.lower() == "exit":
+            break
         
-   
-       title = input("To-do List title : ")
-       if title.lower() == "exit":
+        desc = input("Description: ")
+        if desc.lower() == "exit":
+            break
+        
+        todo_list.append({"title": title, "description": desc})
+    print("To-do item(s) added successfully!\n")
+
+def show_todo_list():
+    if not todo_list:
+        print("No items in the to-do list. \n")
+    else:
+        print("\nYour To-Do List: \n ")
+        for item in todo_list:
+            print(f"Title: {item['title']}")
+            print(f"Description: {item['description']}\n")
+
+# Main loop for the choice menu
+while True:
+    print("1: Add To-Do List \n")
+    print("2: Show All To-Do Lists \n")
+    print("3: Exit \n")
+
+    choice = input("> ")
+
+    if choice == "1":
+        os.system("cls")
+        create_todo_list()
+    elif choice == "2":
+        os.system("cls")
+        show_todo_list()
+    elif choice == "3":
+        os.system("cls")
+        print("Exiting...")
         break
-    
-       desc = input("Description : ")
-       if desc.lower() == "exit":
-        
-           break
-    
-    todo_list.append({ "title":title, "description":desc})
-
-
-create_todo_list()
-
-print(todo_list)
-
-for title in todo_list: 
-    print(title["title"])
+    else:
+        print("Invalid input, please try again.")
